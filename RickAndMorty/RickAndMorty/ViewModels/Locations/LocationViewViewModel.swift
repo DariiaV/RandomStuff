060e8d90db71f5d4cb37a45ab05d58fd6f40a834
Dariia <dariia.grigoreva@gmail.com>
2023-03-12 12:31:40 +0300

    location pagination

--- RickAndMorty/RickAndMorty/ViewModels/Locations/LocationViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Locations/LocationViewViewModel.swift
@@ -25,12 +25,62 @@ final class LocationViewViewModel {
         }
     }
     private(set) var cellViewModels: [LocationTableViewCellViewModel] = []
+    
+    
+    var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil
+    }
+    
     private var apiInfo: GetAllLocationsResponse.Info?
     
     private var hasMoreResults: Bool {
         return false
     }
+    
+    var isLoadingMoreLocations = false
+    
     init() {}
+    
+    ///Paginate if additional locations are needed
+    func fetchAdditionalLocations() {
+        guard !isLoadingMoreLocations else {
+            return
+        }
+        
+        guard let nextUrlString = apiInfo?.next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreLocations = true
+        
+        guard let request = Request(url: url) else {
+            isLoadingMoreLocations = false
+            return
+        }
+        Service.shared.execute(request, expecting: GetAllLocationsResponse.self) { [weak self] result in
+            guard let strongSelf = self else {
+                return
+            }
+            switch result {
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                print("More location: \(moreResults.count)")
+                strongSelf.apiInfo = info
+                strongSelf.cellViewModels.append(contentsOf: moreResults.compactMap({
+                    return LocationTableViewCellViewModel(location: $0)
+                }))
+                DispatchQueue.main.async {
+                    strongSelf.isLoadingMoreLocations = false
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+                self?.isLoadingMoreLocations = false
+            }
+        }
+        
+    }
     
     func location(at index: Int) -> Location? {
         guard index < locations.count, index >= 0 else {
---
+++ RickAndMorty/RickAndMorty/Views/Location/TableLoadingFooterView.swift
@@ -0,0 +1,38 @@
+//
+//  TableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by Дария Григорьева on 12.03.2023.
+//
+
+import UIKit
+
+final class TableLoadingFooterView: UIView {
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubviews(spinner)
+        spinner.startAnimating()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
