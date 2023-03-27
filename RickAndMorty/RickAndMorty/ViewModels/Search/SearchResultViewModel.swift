e534bc4eac1b8738c6c26eb50fcfd4de8ae7e0cb
Dariia <dariia.grigoreva@gmail.com>
2023-03-27 21:51:08 +0300

    SearchResult CollectionView Pagination

--- RickAndMorty/RickAndMorty/ViewModels/Search/SearchResultViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Search/SearchResultViewModel.swift
@@ -38,6 +38,7 @@ final class SearchResultViewModel {
             isLoadingMoreResults = false
             return
         }
+        
         Service.shared.execute(request, expecting: GetAllLocationsResponse.self) { [weak self] result in
             guard let strongSelf = self else {
                 return
@@ -70,7 +71,84 @@ final class SearchResultViewModel {
                 self?.isLoadingMoreResults = false
             }
         }
+    }
+    
+    func fetchAdditionalResults(completion: @escaping ([any Hashable]) -> Void) {
+        guard !isLoadingMoreResults else {
+            return
+        }
         
+        guard let nextUrlString = next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreResults = true
+        
+        guard let request = Request(url: url) else {
+            isLoadingMoreResults = false
+            return
+        }
+        
+        switch results {
+        case .characters(let existingResults):
+            Service.shared.execute(request, expecting: GetAllCharactersResponse.self) { [weak self] result in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    strongSelf.next = info.next //Capture new pagination url
+                    let additionalResults = moreResults.compactMap({
+                        return CharacterCollectionViewCellViewModel(characterName: $0.name, characterStatus: $0.status, characterImageUrl: URL(string: $0.image))
+                    })
+                    var newResults: [CharacterCollectionViewCellViewModel] = []
+                    newResults = existingResults + additionalResults
+                    strongSelf.results = .characters(newResults)
+                    
+                    DispatchQueue.main.async {
+                        strongSelf.isLoadingMoreResults = false
+                        //Notify via callback
+                        completion(newResults)
+                    }
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    self?.isLoadingMoreResults = false
+                }
+            }
+        case .episodes(let existingResults):
+            Service.shared.execute(request, expecting: GetAllEpisodesResponse.self) { [weak self] result in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    strongSelf.next = info.next //Capture new pagination url
+                    let additionalResults = moreResults.compactMap({
+                        return CharacterEpisodeCollectionViewCellViewModel(episodeDataUrl: URL(string: $0.url))
+                    })
+                    var newResults: [CharacterEpisodeCollectionViewCellViewModel] = []
+                    newResults = existingResults + additionalResults
+                    strongSelf.results = .episodes(newResults)
+                    
+                    DispatchQueue.main.async {
+                        strongSelf.isLoadingMoreResults = false
+                        //Notify via callback
+                        completion(newResults)
+                    }
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    self?.isLoadingMoreResults = false
+                }
+            }
+        case .locations:
+            //TableView case
+            break
+        }
     }
 }
 
