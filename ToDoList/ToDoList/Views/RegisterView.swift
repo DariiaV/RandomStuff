41f69cd06b43c773bbebe079324bd769278bec6c
DariiaV <dariia.grigoreva@gmail.com>
2023-07-23 21:14:27 +0300

    add logic register new user

--- ToDoList/ToDoList/Views/RegisterView.swift
+++ ToDoList/ToDoList/Views/RegisterView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @StateObject var viewModel = RegisterViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            //Header
+            HeaderView(title: "Register",
+                       subtitle: "Start organizing todos",
+                       angle: -15,
+                       background: .orange)
+            
+            Form {
+                TextField("Full name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                
+                TLButton(
+                    title: "Create Account",
+                    background: .green
+                ) {
+                    viewModel.register()
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
+            Spacer()
+        }
     }
 }
 
